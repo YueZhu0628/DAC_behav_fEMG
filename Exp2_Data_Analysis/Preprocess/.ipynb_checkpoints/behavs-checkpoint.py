81db621850e3663bbf6a28f16d633c2a583f89ca
Yue Zhu <zhuyue_yolo@163.com>
2024-07-28 13:06:20 -0400

    update

---
+++ Exp2_Data_Analysis/Preprocess/.ipynb_checkpoints/behavs-checkpoint.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# to prepross behavioral data file
+
+
+# In[35]:
+
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+
+
+# In[140]:
+
+
+# load data
+# datafile = "E:\Dissertation\Methods\Exp2_Demand_Choice\Analysis\Exp2_data2\\007\Data\\007_task.csv"
+def load(datafile):
+    col_names = ["PID","Age","Gender","Phase","Block", "TrialN", "NbackLevel", "ISI", 
+                 # Calibration Info
+                   "TrialCountCali", "CaliEnd_A_measure", "ISI_calibrated",
+                 # Trial Info
+                 "TrialIndex", "TrialNumber_total", "TrialNumber_valid",
+                 # Trial Performance
+                 "Trial_A_measure",
+                 "Trial_ACC","Trial_RT",
+                 "Letter_Resp", "Letter_RT", "Letter_Accuracy",
+                 # Learning Phase 
+                 "CueLearning", "CueMapping", "TrialDemandLearning",
+                 # True & False choice
+                 "CueLeft", "CueDemandLeft",
+                 "CueRight", "CueDemandRight",
+                 "PhaseType",
+                 "Choice", "ChoiceRT",
+                 "ChoiceDemand", "TaskDemand",
+                 # Offline Rating T1, T2, T3
+                 "OfflineRatingTimePoint",
+                 "CueFigure", "TrialDemand",
+                 "OfflineRating", "OfflineRatingRT",
+                 # Learning check
+                 "CueCheck", "CueDemandCheck", "CorrectAnswer",
+                 "Answer", "AnswerDemand", "AnswerRT", "AnswerACC",
+                 # Demand Rating
+                 "DemandRatingQuesIndex", "QuesTLX", "DemandLevel", 
+                 "DemandRating", "DemandRatingRT", 
+                 # End
+                 "END"]
+    df = pd.read_csv(datafile, header=0, index_col=False, usecols=col_names)
+    df['TrialN'] = df['TrialN'].fillna(-1).astype(int)
+    return df
+
+
+# In[164]:
+
+
+# separate phases
+def phases(df):
+    def comb_trial_perf(df, phase, cols=["Trial_A_measure","Trial_ACC","Trial_RT"]):
+        df_ct = df.copy().sort_values('TrialN')
+        for col_t in cols:
+            col = f'Task{col_t.replace("Trial","")}'
+            df_ct[col] = df_ct[col_t].shift(1)
+            df_ct_s = df_ct.loc[df_ct['Phase']==phase].sort_values('TrialN')      
+        return df_ct_s
+    df_indi = df[(df['Phase']=="PRACTICE") & (df['TrialN']==1)].copy()
+    df_learnings = df[df['Phase'].isin(["LEARNING","Learning"])].copy().sort_values('TrialN')
+    df_choices = df[df['Phase'].isin(["TrueCHOICE","FalseCHOICE","Choice"])].copy().sort_values('TrialN')
+    df_learning = comb_trial_perf(df_learnings,"LEARNING")
+    df_choice_true = comb_trial_perf(df_choices,"TrueCHOICE")
+    df_choice_false = comb_trial_perf(df_choices,"FalseCHOICE")
+    df_offlines = df[df['Phase'] == "OFFLINERATING"].copy().sort_values('TrialN')
+    df_learning_checks = df[df['Phase'] == "LEARNINGCHECK"].copy().sort_values('TrialN')
+    df_demand_ratings = df[df['Phase'] == "DEMANDRATING"].copy().sort_values('TrialN')
+    
+    # return a dict of dfs
+    df_behavs = {'Individual':df_indi,
+                 'Learning':df_learning, 'TrueChoice':df_choice_true, 'FalseChoice':df_choice_false, 'Offlines':df_offlines,
+                'LearningCheck':df_learning_checks, 'DemandRating':df_demand_ratings}
+    return df_behavs
+
+
+# In[165]:
+
+
+# df = load(datafile)
+# df_behavs = phases(df)
+# check_learn(df_behavs)
+# df_behavs['Learning']['Task_RT']
+
+
+# In[6]:
+
+
+# check for trial numbers
+def check_trial_n(df_behavs):
+    phase_names = ['Learning','TrueChoice','FalseChoice','Offlines']
+    ns = []
+    for phase in phase_names:
+        pn = df_behavs[phase].shape[0]
+        ns.append(pn)
+    if ns == [50,10,50,8]:
+        print("Trial Numbers Correct!")
+    else:
+        print("Error!", phase, ns)
+
+
+# In[32]:
+
+
+# check for whether learned the association
+def check_learn(df_behavs):
+    df = df_behavs['LearningCheck']
+    pid = df['PID']
+    df_check = df[['CueDemandCheck','CorrectAnswer','AnswerDemand','AnswerACC']]
+    learn = df_check.loc[df_check['CorrectAnswer'].isin(["Left","Right"]),'AnswerACC'].mean(axis=0)
+    delearn = df_check.loc[df_check['CorrectAnswer']=="Middle",'AnswerACC'].mean(axis=0)
+    if (learn == 1) & (delearn == 1):
+        pass
+        # print("Learned well!")
+    elif (learn == 1) & (delearn != 1):
+        print(pid, "Learned well! Not De-Learned!")
+    elif (learn != 1) & (delearn == 1):
+        print(pid, "Not Learned! De-Learned well!")
+    else:
+        print(pid, "Fail both!")
+
+
+# In[46]:
+
+
+# standardize values
+def stand_rt(df_behavs):
+    for phase in ['Learning','TrueChoice','FalseChoice']:
+        df = df_behavs[phase]
+        df['Task_RT_z'] = stats.zscore(df['Task_RT'],nan_policy='omit')
+        df['Trial_RT_log'] = np.log(df['Task_RT'])
+        df['Trial_RT_log_z'] = stats.zscore(df['Trial_RT_log'],nan_policy='omit')
+
+
+# In[47]:
+
+
+# standardize values
+def stand_rating(df_behavs):
+    for phase in ['Offlines','DemandRating']:
+        df = df_behavs[phase]
+        if phase == 'Offlines':
+            col = 'OfflineRating'
+        else:
+            col = 'DemandRating'
+        df[f'{col}_z'] = stats.zscore(df[col],nan_policy='omit')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[135]:
+
+
+# df_behavs['Individual'][['PID','Age','Gender','ISI', 'CueMapping']]
+# stand_rt(df_behavs)
+# df_behavs['Learning'][['Trial_RT','Trial_RT_z','Trial_RT_log','Trial_RT_log_z']]
+# stand_rating(df_behavs)
+# df_behavs['Offlines']['OfflineRating_z']
+# df_behavs['DemandRating'][['DemandLevel','DemandRating_z']]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
--- Exp2_Data_Analysis/Preprocess/pyphysio/pyphysio/specialized/eda/_presets.py
+++ Exp2_Data_Analysis/Preprocess/pyphysio/pyphysio/specialized/eda/_presets.py
@@ -13,7 +13,25 @@ def preset_phasic(delta, prefix="pha_"):
     pks_mean = PeaksMean(delta=delta)
     n_peaks = PeaksNum(delta=delta)
     dur_mean = DurationMean(delta=delta, win_pre=2, win_post=2)
-    slopes_mean = SlopeMean(delta=delta, win_pre=2, win_post=2)
+    """
+    DurationMean function:
+
+    Parameters
+    ----------
+    delta : float, >0
+        Minimum amplitude of peaks to be selected
+    win_pre : float, >0, default=1
+        Interval before a detected peak where to search the start of the peak
+    win_post : float, >0, default=1
+        Interval after a detected peak where to search the end of the peak
+
+    Returns
+    -------
+    av : float
+        Average duration of detected peaks
+
+    """
+    slopes_mean = SlopeMean(delta=delta, win_pre=1.5, win_post=1.5)
     auc = AUC()
 
     t = [mean, std, rng, pks_max, pks_min, pks_mean, n_peaks, dur_mean, slopes_mean, auc]
